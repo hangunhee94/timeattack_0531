142f88b4116813334a4a247a51117f395f91dfb1
hangunhee94 <webzen9422@gmail.com>
2022-05-31 09:40:09 +0900

    Add : CategoryModel ArticleModel

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class CategoryModel(models.Model):
+    class Meta:
+        db_table = "my_category"
+
+    category = models.CharField(max_length=256)
+    description = models.CharField(max_length=256, default='')
+
+class ArticleModel(models.Model):
+    class Meta:
+        db_table = "my_blog"
+
+    title = models.CharField(max_length=256)
+    category = models.ForeignKey(CategoryModel, on_delete=models.CASCADE)
+    content = models.CharField(max_length=256)
